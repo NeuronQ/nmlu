2476d42834a6a89d40a5598ecc5e1c60a3c1df29
NeuronQ <io@neuronq.ro>
2019-01-06 19:47:35 +0100

    let there be light

---
+++ example_etl.py
@@ -0,0 +1,18 @@
+from subprocess import call
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from qinspect.console import df_peek
+
+
+INPUT_DIR = './example_data/titanic/'
+
+print("\n--- INPUT:")
+call(['ls', '-lah', INPUT_DIR])
+
+# load
+df_raw = pd.read_csv(f'{INPUT_DIR}train.csv', low_memory=False)
+
+# Peek
+df_peek(df_raw, 'df_raw')
